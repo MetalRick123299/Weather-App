30b63db7be20e23c738a7166d9943b0de60937a9
Richard <richard031104@outlook.com>
2022-07-28 11:53:52 -0400

    First Commit

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+/* eslint-disable */
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'dist'),
+    },
+    port: 8080,
+    open: true,
+    hot: true,
+    compress: true,
+  },
+};
